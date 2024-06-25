02b8d88c435da4fc3fd70c71d9ec272850dec3c5
ChanGil <chan.gil1837@gmail.com>
2024-06-25 12:52:13 +0900

    Method change for next.js 14

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,4 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import RootLayout from '../layout';
-import ProjectItem from './project-item';
+import ProjectItem from "./project-item";
 
 type Project = {
   id: string;
@@ -13,25 +11,7 @@ type Project = {
   tags: Array<{ name: string }>;
 };
 
-const Projects = ({ projects }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return (
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-20">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">프로젝트</h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">여태 수행한 프로젝트 목록입니다. </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 sm:w-full">
-          {projects.map((project: Project) => (
-            <ProjectItem key={project.id} project={project} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps<{ projects: Project[] }> = async () => {
+async function getProjects(): Promise<Project[]> {
   const res = await fetch(process.env.NEXT_PUBLIC_NOTION_DATABASE!, {
     cache: 'no-store',
     method: 'POST',
@@ -53,7 +33,7 @@ export const getServerSideProps: GetServerSideProps<{ projects: Project[] }> = a
   });
 
   const data = await res.json();
-  const projects = data.results.map((ctx: any) => {
+  return data.results.map((ctx: any) => {
     const title = ctx.properties?.Name?.title?.[0]?.text?.content || 'No Title';
     const description = ctx.properties?.Description?.rich_text?.[0]?.text?.content || 'No Description';
     const cover = ctx.cover?.file?.url || '';
@@ -73,12 +53,24 @@ export const getServerSideProps: GetServerSideProps<{ projects: Project[] }> = a
       tags
     };
   });
+}
 
-  return {
-    props: {
-      projects,
-    },
-  };
-};
+export default async function Projects() {
+  const projects = await getProjects();
 
-export default Projects;
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">프로젝트</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">여태 수행한 프로젝트 목록입니다. </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 sm:w-full">
+          {projects.map((project: Project) => (
+            <ProjectItem key={project.id} project={project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
