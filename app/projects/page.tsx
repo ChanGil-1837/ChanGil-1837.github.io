fd21679633a9bfa72eed5b5b926a11e8ffafc47b
ChanGil <chan.gil1837@gmail.com>
2024-06-25 13:10:39 +0900

    Error fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -37,14 +37,18 @@ async function getProjects(): Promise<Project[]> {
   }
 
   const data = await res.json();
+  if (!data.results) {
+    throw new Error('No results found in the response');
+  }
+
   return data.results.map((ctx: any) => {
     const title = ctx.properties?.Name?.title?.[0]?.text?.content || 'No Title';
     const description = ctx.properties?.Description?.rich_text?.[0]?.text?.content || 'No Description';
     const cover = ctx.cover?.file?.url || '';
     const start = ctx.properties?.['Work period']?.date?.start || 'No Start Date';
     const end = ctx.properties?.['Work period']?.date?.end || 'No End Date';
-    const link = ctx.properties?.link?.url || 'No End Date';
-    const tags = ctx.properties?.Tags?.multi_select.map((tag: any) => ({ name: tag.name }));
+    const link = ctx.properties?.link?.url || 'No Link';
+    const tags = ctx.properties?.Tags?.multi_select.map((tag: any) => ({ name: tag.name })) || [];
 
     return {
       id: ctx.id,
@@ -88,7 +92,6 @@ export default async function Projects() {
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">프로젝트</h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">여태 수행한 프로젝트 목록입니다. </p>
         </div>
-        ${process.env.NEXT_PUBLIC_NOTION_DATABASE}
         <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 sm:w-full">
           {projects.map((project: Project) => (
             <ProjectItem key={project.id} project={project} />
