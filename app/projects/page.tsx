c417ab0ea59c400e69a9640839430a716e241ecb
ChanGil <chan.gil1837@gmail.com>
2024-06-25 12:50:09 +0900

    Method change

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,66 +1,84 @@
-import RootLayout from "../layout";
-import ProjectItem from "./project-item";
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import RootLayout from '../layout';
+import ProjectItem from './project-item';
 
-export default async function Projects() {
-    const projects = await getPost();
+type Project = {
+  id: string;
+  title: string;
+  description: string;
+  cover: string;
+  start: string;
+  end: string;
+  link: string;
+  tags: Array<{ name: string }>;
+};
 
-    return (
-        
-        <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-col text-center w-full mb-20">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">프로젝트</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">여태 수행한 프로젝트 목록입니다. </p>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 sm:w-full" >  
-                    {projects.map((project:any, index:any) => (
-                        <ProjectItem key={project.id} project={project} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
-}
+const Projects = ({ projects }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">프로젝트</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">여태 수행한 프로젝트 목록입니다. </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 sm:w-full">
+          {projects.map((project: Project) => (
+            <ProjectItem key={project.id} project={project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export async function getPost() {
+export const getServerSideProps: GetServerSideProps<{ projects: Project[] }> = async () => {
+  const res = await fetch(process.env.NEXT_PUBLIC_NOTION_DATABASE!, {
+    cache: 'no-store',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTION_TOKEN}`
+    },
+    body: JSON.stringify({
+      page_size: 100,
+      sorts: [
+        {
+          property: 'Work period',
+          direction: 'ascending'
+        }
+      ]
+    }),
+  });
 
-    const res = await fetch(process.env.NEXT_PUBLIC_NOTION_DATABASE, {
-        cache: "no-store",
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Notion-Version': '2022-06-28',
-            'Accept': 'application/json',
-            'Authorization': process.env.NEXT_PUBLIC_NOTION_TOKEN
-        },
-        body: JSON.stringify({
-            page_size: 100,
-            sorts: [
-                {
-                    property: 'Work period',
-                    direction: 'ascending'
-                }
-            ]
-        }),
-    });
+  const data = await res.json();
+  const projects = data.results.map((ctx: any) => {
+    const title = ctx.properties?.Name?.title?.[0]?.text?.content || 'No Title';
+    const description = ctx.properties?.Description?.rich_text?.[0]?.text?.content || 'No Description';
+    const cover = ctx.cover?.file?.url || '';
+    const start = ctx.properties?.['Work period']?.date?.start || 'No Start Date';
+    const end = ctx.properties?.['Work period']?.date?.end || 'No End Date';
+    const link = ctx.properties?.link?.url || 'No End Date';
+    const tags = ctx.properties?.Tags?.multi_select.map((tag: any) => ({ name: tag.name }));
 
-    const data = await res.json();
-    const projects = data.results.map((ctx:any) => {
-        const title = ctx.properties?.Name?.title?.[0]?.text?.content || 'No Title';
-        const description = ctx.properties?.Description?.rich_text?.[0]?.text?.content || 'No Description';
-        const cover = ctx.cover.file.url
-        const start = ctx.properties?.['Work period']?.date?.start || 'No Start Date';
-        const end = ctx.properties?.['Work period']?.date?.end || 'No End Date';
-        const link = ctx.properties?.link?.url || 'No End Date';
-        const tags = ctx.properties?.Tags?.multi_select
-        return {
-            id: ctx.id,
-            title,
-            description,
-            cover,
-            start,end,link,tags
-        };
-    });
-    console.log(projects)
-    return projects;
-}
+    return {
+      id: ctx.id,
+      title,
+      description,
+      cover,
+      start,
+      end,
+      link,
+      tags
+    };
+  });
+
+  return {
+    props: {
+      projects,
+    },
+  };
+};
+
+export default Projects;
